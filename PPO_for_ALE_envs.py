76af071823297e715787cec24ad4d6f387661593
Raven4567 <128988633+Raven4567@users.noreply.github.com>
2025-02-05 07:17:13 +0300

    The small fix.

--- PPO_for_ALE_envs.py
+++ PPO_for_ALE_envs.py
@@ -18,12 +18,12 @@ class Memory:
         self.log_probs = []
 
     def push(self, state, action, reward, done, value, log_prob):
-        self.states.append(np.array(state), dtype=np.float32)
-        self.actions.append(np.array(action), dtype=np.float32)
-        self.rewards.append(np.array(reward), dtype=np.float32)
-        self.dones.append(np.array(done), dtype=np.float32)
-        self.state_values.append(np.array(value), dtype=np.float32)
-        self.log_probs.append(np.array(log_prob), dtype=np.float32)
+        self.states.append(np.array(state, dtype=np.float32))
+        self.actions.append(np.array(action, dtype=np.float32))
+        self.rewards.append(np.array(reward, dtype=np.float32))
+        self.dones.append(np.array(done, dtype=np.float32))
+        self.state_values.append(np.array(value, dtype=np.float32))
+        self.log_probs.append(np.array(log_prob, dtype=np.float32))
 
     def clear(self):
         del self.states[:]
